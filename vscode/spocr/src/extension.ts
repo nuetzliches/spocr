df3b981aaeb074764e7343f699d133c53e923de7
Sebastian Gieseler <sgieseler@live.de>
2018-10-13 14:30:22 +0200

    init spocr vscode extension

--- vscode/spocr/src/extension.ts
+++ vscode/spocr/src/extension.ts
@@ -18,10 +18,16 @@ export function activate(context: vscode.ExtensionContext) {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        // vscode.window.showInformationMessage('Hello Ext!');
+
+        // vscode.window.showSaveDialog({ } as SaveDialogOptions);
+
+        vscode.workspace.findFiles('spocr.json').then(i => i.forEach(f => console.log('JOOOOO -> ', f)));
     });
 
     context.subscriptions.push(disposable);
+
+    // 
 }
 
 // this method is called when your extension is deactivated
