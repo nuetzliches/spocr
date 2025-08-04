a20131823d0ccb215858a680e022876d9191d475
Sebastian Gieseler <sebastian@nuetzliche.it>
2025-08-04 11:55:25 +0200

    feat(db-context) make options optional

--- src/Output-v9-0/DataContext/AppDbContextExtensions.base.cs
+++ src/Output-v9-0/DataContext/AppDbContextExtensions.base.cs
@@ -7,107 +7,106 @@ using System.Threading;
 using System.Threading.Tasks;
 using Source.DataContext.Outputs;
 
-namespace Source.DataContext
+namespace Source.DataContext;
+
+public static class AppDbContextExtensions
 {
-    public static class AppDbContextExtensions
+    public static IAppDbContextPipe WithCommandTimeout(this IAppDbContext context, int commandTimeout)
     {
-        public static IAppDbContextPipe WithCommandTimeout(this IAppDbContext context, int commandTimeout)
-        {
-            return context.CreatePipe().WithCommandTimeout(commandTimeout);
-        }
-
-        public static IAppDbContextPipe WithTransaction(this IAppDbContext context, SqlTransaction transaction)
-        {
-            return context.CreatePipe().WithTransaction(transaction);
-        }
-
-        public static IAppDbContextPipe CreatePipe(this IAppDbContext context)
-        {
-            return new AppDbContextPipe(context).WithCommandTimeout(context.Options.CommandTimeout);
-        }
+        return context.CreatePipe().WithCommandTimeout(commandTimeout);
     }
 
-    public static class AppDbContextPipeExtensions
+    public static IAppDbContextPipe WithTransaction(this IAppDbContext context, SqlTransaction transaction)
     {
-        public static IAppDbContextPipe WithCommandTimeout(this IAppDbContextPipe pipe, int commandTimeout)
+        return context.CreatePipe().WithTransaction(transaction);
+    }
+
+    public static IAppDbContextPipe CreatePipe(this IAppDbContext context)
+    {
+        return new AppDbContextPipe(context).WithCommandTimeout(context.Options.CommandTimeout);
+    }
+}
+
+public static class AppDbContextPipeExtensions
+{
+    public static IAppDbContextPipe WithCommandTimeout(this IAppDbContextPipe pipe, int commandTimeout)
+    {
+        pipe.CommandTimeout = commandTimeout;
+        return pipe;
+    }
+
+    public static IAppDbContextPipe WithTransaction(this IAppDbContextPipe pipe, SqlTransaction transaction)
+    {
+        pipe.Transaction = transaction;
+        return pipe;
+    }
+
+    public static async Task<TOutput> ExecuteAsync<TOutput>(this IAppDbContextPipe pipe, string procedureName, IEnumerable<SqlParameter> parameters, CancellationToken cancellationToken = default) where TOutput : class, IOutput, new()
+    {
+        var command = await pipe.CreateSqlCommandAsync(procedureName, parameters, cancellationToken);
+
+        await command.ExecuteNonQueryAsync(cancellationToken);
+
+        return parameters.ToOutput<TOutput>();
+    }
+
+    public static async Task<List<T>> ExecuteListAsync<T>(this IAppDbContextPipe pipe, string procedureName, IEnumerable<SqlParameter> parameters, CancellationToken cancellationToken = default) where T : class, new()
+    {
+        var command = await pipe.CreateSqlCommandAsync(procedureName, parameters, cancellationToken);
+
+        var result = new List<T>();
+
+        var reader = await command.ExecuteReaderAsync(cancellationToken);
+        while (await reader.ReadAsync(cancellationToken)) result.Add(reader.ConvertToObject<T>());
+        reader.Close();
+
+        return result;
+    }
+
+    public static async Task<T> ExecuteSingleAsync<T>(this IAppDbContextPipe pipe, string procedureName, IEnumerable<SqlParameter> parameters, CancellationToken cancellationToken = default) where T : class, new()
+    {
+        return (await pipe.ExecuteListAsync<T>(procedureName, parameters, cancellationToken)).SingleOrDefault();
+    }
+
+    public static async Task<string> ReadJsonAsync(this IAppDbContextPipe pipe, string procedureName, IEnumerable<SqlParameter> parameters, CancellationToken cancellationToken = default)
+    {
+        var command = await pipe.CreateSqlCommandAsync(procedureName, parameters, cancellationToken);
+
+        var result = new StringBuilder();
+        var reader = await command.ExecuteReaderAsync(cancellationToken);
+        while (reader.Read()) result.Append(reader.GetValue(0).ToString());
+        reader.Close();
+
+        return result.ToString();
+    }
+
+    internal static async Task<SqlCommand> CreateSqlCommandAsync(this IAppDbContextPipe pipe, string procedureName, IEnumerable<SqlParameter> parameters, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        if (pipe.Context.Connection.State != ConnectionState.Open) await pipe.Context.Connection.OpenAsync(cancellationToken);
+
+        var command = new SqlCommand(procedureName, pipe.Context.Connection)
         {
-            pipe.CommandTimeout = commandTimeout;
-            return pipe;
+            CommandType = CommandType.StoredProcedure,
+            Transaction = pipe.GetCurrentTransaction()
+        };
+
+        if (pipe.CommandTimeout.HasValue)
+        {
+            command.CommandTimeout = pipe.CommandTimeout.Value;
         }
 
-        public static IAppDbContextPipe WithTransaction(this IAppDbContextPipe pipe, SqlTransaction transaction)
+        if (parameters?.Any() ?? false)
         {
-            pipe.Transaction = transaction;
-            return pipe;
+            command.Parameters.AddRange(parameters.ToArray());
         }
 
-        public static async Task<TOutput> ExecuteAsync<TOutput>(this IAppDbContextPipe pipe, string procedureName, IEnumerable<SqlParameter> parameters, CancellationToken cancellationToken = default) where TOutput : class, IOutput, new()
-        {
-            var command = await pipe.CreateSqlCommandAsync(procedureName, parameters, cancellationToken);
+        return command;
+    }
 
-            await command.ExecuteNonQueryAsync(cancellationToken);
-
-            return parameters.ToOutput<TOutput>();
-        }
-
-        public static async Task<List<T>> ExecuteListAsync<T>(this IAppDbContextPipe pipe, string procedureName, IEnumerable<SqlParameter> parameters, CancellationToken cancellationToken = default) where T : class, new()
-        {
-            var command = await pipe.CreateSqlCommandAsync(procedureName, parameters, cancellationToken);
-
-            var result = new List<T>();
-
-            var reader = await command.ExecuteReaderAsync(cancellationToken);
-            while (await reader.ReadAsync(cancellationToken)) result.Add(reader.ConvertToObject<T>());
-            reader.Close();
-
-            return result;
-        }
-
-        public static async Task<T> ExecuteSingleAsync<T>(this IAppDbContextPipe pipe, string procedureName, IEnumerable<SqlParameter> parameters, CancellationToken cancellationToken = default) where T : class, new()
-        {
-            return (await pipe.ExecuteListAsync<T>(procedureName, parameters, cancellationToken)).SingleOrDefault();
-        }
-
-        public static async Task<string> ReadJsonAsync(this IAppDbContextPipe pipe, string procedureName, IEnumerable<SqlParameter> parameters, CancellationToken cancellationToken = default)
-        {
-            var command = await pipe.CreateSqlCommandAsync(procedureName, parameters, cancellationToken);
-
-            var result = new StringBuilder();
-            var reader = await command.ExecuteReaderAsync(cancellationToken);
-            while (reader.Read()) result.Append(reader.GetValue(0).ToString());
-            reader.Close();
-
-            return result.ToString();
-        }
-
-        internal static async Task<SqlCommand> CreateSqlCommandAsync(this IAppDbContextPipe pipe, string procedureName, IEnumerable<SqlParameter> parameters, CancellationToken cancellationToken = default)
-        {
-            cancellationToken.ThrowIfCancellationRequested();
-
-            if (pipe.Context.Connection.State != ConnectionState.Open) await pipe.Context.Connection.OpenAsync(cancellationToken);
-
-            var command = new SqlCommand(procedureName, pipe.Context.Connection)
-            {
-                CommandType = CommandType.StoredProcedure,
-                Transaction = pipe.GetCurrentTransaction()
-            };
-
-            if (pipe.CommandTimeout.HasValue)
-            {
-                command.CommandTimeout = pipe.CommandTimeout.Value;
-            }
-
-            if (parameters?.Any() ?? false)
-            {
-                command.Parameters.AddRange(parameters.ToArray());
-            }
-
-            return command;
-        }
-
-        internal static SqlTransaction GetCurrentTransaction(this IAppDbContextPipe pipe)
-        {
-            return pipe.Transaction ?? pipe.Context.Transactions.LastOrDefault();
-        }
+    internal static SqlTransaction GetCurrentTransaction(this IAppDbContextPipe pipe)
+    {
+        return pipe.Transaction ?? pipe.Context.Transactions.LastOrDefault();
     }
 }
--- src/Output-v9-0/DataContext/SqlParameterExtensions.base.cs
+++ src/Output-v9-0/DataContext/SqlParameterExtensions.base.cs
@@ -9,110 +9,109 @@ using System.Linq;
 using System.Reflection;
 using Source.DataContext.Outputs;
 
-namespace Source.DataContext
+namespace Source.DataContext;
+
+public static class SqlParameterExtensions
 {
-    public static class SqlParameterExtensions
+    public static IEnumerable<SqlDataRecord> ToSqlParamCollection<T>(this T value)
     {
-        public static IEnumerable<SqlDataRecord> ToSqlParamCollection<T>(this T value)
+        var collection = new List<SqlDataRecord>();
+
+        var list = value as IEnumerable;
+        // scalar value is allowed
+        if (list == null && value != null)
         {
-            var collection = new List<SqlDataRecord>();
-
-            var list = value as IEnumerable;
-            // scalar value is allowed
-            if (list == null && value != null)
-            {
-                // create single row of type<T>
-                list = new List<T> { value };
-            }
-
-            foreach (var row in list)
-            {
-                var rowType = row.GetType();
-                var properties = rowType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
-
-                var metas = new List<SqlMetaData>();
-                var values = new List<object>();
-                foreach (var property in properties)
-                {
-                    var propVal = property.GetValue(row);
-                    var propName = property.Name;
-                    var sqlType = AppDbContext.GetSqlDbType(property.PropertyType);
-
-                    if (sqlType == SqlDbType.NVarChar || sqlType == SqlDbType.VarBinary)
-                    {
-                        var maxLengthAttribute = (MaxLengthAttribute)property.GetCustomAttributes(typeof(MaxLengthAttribute), false).FirstOrDefault();
-                        if (maxLengthAttribute != null)
-                        {
-                            metas.Add(new SqlMetaData(propName, sqlType, maxLengthAttribute?.Length ?? 0));
-                        }
-                        else
-                        {
-                            metas.Add(new SqlMetaData(propName, sqlType, SqlMetaData.Max));
-                        }
-                    }
-                    else if (sqlType == SqlDbType.Decimal)
-                    {
-                        metas.Add(new SqlMetaData(propName, sqlType, 18, 4));
-                    }
-                    else
-                    {
-                        metas.Add(new SqlMetaData(propName, sqlType));
-                    }
-                    values.Add(propVal);
-                }
-                var record = new SqlDataRecord(metas.ToArray());
-                record.SetValues(values.ToArray());
-                collection.Add(record);
-            }
-
-            // If there are no records in the collection, use a null reference for the value instead.
-            return collection.Count > 0
-                ? collection
-                : null;
+            // create single row of type<T>
+            list = new List<T> { value };
         }
 
-        public static TOutput ToOutput<TOutput>(this IEnumerable<SqlParameter> parameters) where TOutput : class, IOutput, new()
+        foreach (var row in list)
         {
-            var result = new TOutput();
-
-            var outputs = parameters.ToList().Where(p => p.Direction == ParameterDirection.Output || p.Direction == ParameterDirection.InputOutput);
-
-            var resultType = result.GetType();
-            var properties = resultType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var rowType = row.GetType();
+            var properties = rowType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
 
             var metas = new List<SqlMetaData>();
             var values = new List<object>();
-            foreach (var output in outputs)
+            foreach (var property in properties)
             {
-                var parameterName = output.ParameterName.Replace("@", "");
-                var property = properties.FirstOrDefault(p => p.Name.Equals(parameterName));
-                if (property == null || output.Value == DBNull.Value)
-                {
-                    continue;
-                }
+                var propVal = property.GetValue(row);
+                var propName = property.Name;
+                var sqlType = AppDbContext.GetSqlDbType(property.PropertyType);
 
-                property.SetValue(result, output.Value);
-            }
-
-            // try to add recordId from context
-            try
-            {
-                var contextParameter = parameters.FirstOrDefault(p => p.ParameterName.Replace("@", "").Equals("Context"));
-                if (contextParameter != null && result.RecordId == null)
+                if (sqlType == SqlDbType.NVarChar || sqlType == SqlDbType.VarBinary)
                 {
-                    var contextRecord = (contextParameter.Value as List<SqlDataRecord>)?.FirstOrDefault();
-                    var recordIdColumn = contextRecord.GetOrdinal("RecordId");
-                    var recordId = contextRecord?.GetValue(recordIdColumn);
-                    if (recordId != DBNull.Value)
+                    var maxLengthAttribute = (MaxLengthAttribute)property.GetCustomAttributes(typeof(MaxLengthAttribute), false).FirstOrDefault();
+                    if (maxLengthAttribute != null)
                     {
-                        var recordIdProperty = properties.FirstOrDefault(p => p.Name.Equals("RecordId"));
-                        recordIdProperty.SetValue(result, recordId);
+                        metas.Add(new SqlMetaData(propName, sqlType, maxLengthAttribute?.Length ?? 0));
+                    }
+                    else
+                    {
+                        metas.Add(new SqlMetaData(propName, sqlType, SqlMetaData.Max));
                     }
                 }
+                else if (sqlType == SqlDbType.Decimal)
+                {
+                    metas.Add(new SqlMetaData(propName, sqlType, 18, 4));
+                }
+                else
+                {
+                    metas.Add(new SqlMetaData(propName, sqlType));
+                }
+                values.Add(propVal);
             }
-            catch { }
-
-            return result;
+            var record = new SqlDataRecord(metas.ToArray());
+            record.SetValues(values.ToArray());
+            collection.Add(record);
         }
+
+        // If there are no records in the collection, use a null reference for the value instead.
+        return collection.Count > 0
+            ? collection
+            : null;
+    }
+
+    public static TOutput ToOutput<TOutput>(this IEnumerable<SqlParameter> parameters) where TOutput : class, IOutput, new()
+    {
+        var result = new TOutput();
+
+        var outputs = parameters.ToList().Where(p => p.Direction == ParameterDirection.Output || p.Direction == ParameterDirection.InputOutput);
+
+        var resultType = result.GetType();
+        var properties = resultType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        var metas = new List<SqlMetaData>();
+        var values = new List<object>();
+        foreach (var output in outputs)
+        {
+            var parameterName = output.ParameterName.Replace("@", "");
+            var property = properties.FirstOrDefault(p => p.Name.Equals(parameterName));
+            if (property == null || output.Value == DBNull.Value)
+            {
+                continue;
+            }
+
+            property.SetValue(result, output.Value);
+        }
+
+        // try to add recordId from context
+        try
+        {
+            var contextParameter = parameters.FirstOrDefault(p => p.ParameterName.Replace("@", "").Equals("Context"));
+            if (contextParameter != null && result.RecordId == null)
+            {
+                var contextRecord = (contextParameter.Value as List<SqlDataRecord>)?.FirstOrDefault();
+                var recordIdColumn = contextRecord.GetOrdinal("RecordId");
+                var recordId = contextRecord?.GetValue(recordIdColumn);
+                if (recordId != DBNull.Value)
+                {
+                    var recordIdProperty = properties.FirstOrDefault(p => p.Name.Equals("RecordId"));
+                    recordIdProperty.SetValue(result, recordId);
+                }
+            }
+        }
+        catch { }
+
+        return result;
     }
 }
