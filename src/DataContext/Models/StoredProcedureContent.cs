fc9fccdf95f1b16772268e9f73eb9a376782b512
Sebastian Gieseler <sebastian@nuetzliche.it>
2025-10-29 15:30:35 +0100

    Remove DataContext and related models and queries

--- src/DataContext/Models/StoredProcedureContent.cs
+++
@@ -1,9 +0,0 @@
-using SpocR.DataContext.Attributes;
-
-namespace SpocR.DataContext.Models;
-
-public class StoredProcedureContent
-{
-    [SqlFieldName("definition")]
-    public string Definition { get; set; }
-}
